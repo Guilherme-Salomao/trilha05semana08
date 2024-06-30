92b32c6fbe9298a3ef252261352af3d83595a9df
Guilherme-Salomao <vitrineata@vitrineata.com.br>
2024-06-30 14:18:39 -0300

    Iniciando Semana 08

---
+++ Modelos/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana08.Modelos
+{
+    internal class Endereco
+    {
+        string bairro;
+        string cidade;
+        string complemento;
+        string estado;
+        string rua;
+        int numero;
+    }
+}
---
+++ Modelos/ItemDePedido.cs
@@ -0,0 +1,30 @@
+﻿using Semana08.Classes;
+using System;
+
+namespace Semana08.Modelos
+{
+    internal class ItemDePedido : Produto
+    {
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+        public decimal Subtotal
+        {
+            get { return Quantidade * PrecoUnitario; }
+        }
+
+        // Construtor
+        public ItemDePedido(string nome, int quantidade, decimal precoUnitario) : base(nome)
+        {
+            Quantidade = quantidade;
+            PrecoUnitario = precoUnitario;
+        }
+
+        public string ObterItemProduto()
+        {
+            return $"Nome do Produto: {Nome}, Preço Unitário: {PrecoUnitario} -- Quantidade: {Quantidade} -- Valor Total: {Subtotal}";
+        }
+    }
+}
+
+
+
